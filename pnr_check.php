4bca5b199e3fc320e7f2fb9967b9cdfc0e0ca30f
Harshita65 <harshitashrip@karunya.edu.in>
2021-10-08 14:21:32 +0530

    new commit

--- pnr_check.php
+++ pnr_check.php
@@ -1,21 +1,21 @@
 
 <html>
     <head>
-        <link rel="stylesheet" href="assets/style/style.css">
+        <link rel='stylesheet' href='assets/style/style.css'>
     </head>
-    <body class="rail-bg6">
+    <body class='rail-bg6'>
             
-                <div id="mytopnav" class = "topnav">
-                    <div id="navtext"><a href="Railway.html" >Home</a></div> 
-                    <div id="navtext"><a href="Railway_PNR.html">PNR Enquiry</a></div>
-                    <div id="navtext"><a href="Railway_tickets.html">Book Tickets</a></div>
-                    <div id="navtext"><a href="#Freight Services">Freight Services</a></div>
-                    <div id="navtext"><a href="railway_register.php" >Login/Register</a></div>
+                <div id='mytopnav' class = 'topnav'>
+                    <div id='navtext'><a href='Railway.html' >Home</a></div> 
+                    <div id='navtext'><a href='Railway_PNR.html'>PNR Enquiry</a></div>
+                    <div id='navtext'><a href='Railway_tickets.html'>Book Tickets</a></div>
+                    <div id='navtext'><a href='#Freight Services'>Freight Services</a></div>
+                    <div id='navtext'><a href='railway_register.php' >Login/Register</a></div>
                 </div>
             
-        <div id ="pnrcheck_glass">    
-        <h2 id="pnrcheck_heading">Passenger Current status</h2>
-        <table id="table">
+        <div id ='pnrcheck_glass'>    
+        <h2 id='pnrcheck_heading'>Passenger Current status</h2>
+        <table id='table'>
             <tr>
                 <th>Train Number</th>
                 <th>Train Name</th>
